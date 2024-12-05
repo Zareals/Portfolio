64a582c655c4adbe705b75e7975bd2299a459343
Nassef <kenwayconnor505@gmail.com>
2024-12-05 20:50:03 +0100

    fixed lint

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsCameraVideo, BsGithub } from "react-icons/bs";
+import { BsCameraVideo } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
